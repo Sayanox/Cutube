a50120fc40e329eac59af1e735436999118b50ba
Almuntaser448 <77934756+Almuntaser448@users.noreply.github.com>
2021-03-18 16:17:56 +0100

    Add files via upload

---
+++ train/app/src/main/java/com/example/Cutube/controller/UrlActivity.java
@@ -0,0 +1,16 @@
+package com.example.cutube.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cutube.R;
+
+public class UrlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_url);
+    }
+}
